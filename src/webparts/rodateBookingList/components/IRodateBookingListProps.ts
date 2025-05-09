e35f594c7901d799e6181a4ee183e44350ea0faa
Pablo Gamarra <78704427+pablolgamarra@users.noreply.github.com>
2025-05-09 16:03:32 -0300

    feat: remove unused files

--- src/webparts/rodateBookingList/components/IRodateBookingListProps.ts
+++
@@ -1,7 +0,0 @@
-export interface IRodateBookingListProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-}
